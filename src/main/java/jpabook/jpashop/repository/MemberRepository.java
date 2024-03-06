11cf5bd13296b0b1a3dc357daf3761dba4f56944
0921sean <0921sean@gmail.com>
2024-03-06 21:48:43 +0900

    [03.06 승범] domain 작성

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+}
